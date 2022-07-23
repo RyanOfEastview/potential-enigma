2057193286669942a72aebef250ea538a1f97b09
RyanOfEastview <ryanmccarthy613@gmail.com>
2022-07-23 15:04:24 -0400

    created generate markdown function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,7 +12,42 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  
+  # ${data.projectTitle}
+
+  ![badge](https://img.shields.io/badge/license-${data.license}-brightgreen)<br />
+
+  ## Description
+    ${data.description}
+
+  ## Table of Contents
+    - [Description](#description)
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Contribution](#contribution)
+    - [Tests](#tests)
+    - [Questions](#questions)
+    - [License](#license)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Contributions
+  ${data.contribution}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  ${data.questions}
+  Reach out with any questions you may have to: ${answers.email}
+
+  ## License
+  This applicatinos uses the ${data.license} license.
 
 `;
 }
